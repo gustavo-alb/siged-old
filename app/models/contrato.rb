94619ffa421c8c8823f1fc9a425112491342e890
Gustavo Albuquerque <alucard.ap@gmail.com>
2016-03-29 09:31:47 -0300

    desabilitar audited

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -2,5 +2,5 @@ class Contrato < ActiveRecord::Base
   belongs_to :funcionario
   belongs_to :lotacao
   validates_uniqueness_of :funcionario_id,:scope=>[:lotacao_id]
-  audited :associated_with => :funcionario
+  #audited :associated_with => :funcionario
 end
